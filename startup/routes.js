67d6e59f6c7a9ca96110d9313170bb5d091fd00e
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-30 13:15:22 -0400

    fixing path4

--- startup/routes.js
+++ startup/routes.js
@@ -30,11 +30,11 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
+    app.use(express.static('build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
   }
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
   app.use('/api/auth', auth);
   app.use(attachUser);
   app.use(verifyJwt);
