4d0769c5a431176f2667ea16ab136c9271b25e47
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-09-30 13:21:31 -0400

    fixing path5

--- startup/routes.js
+++ startup/routes.js
@@ -30,9 +30,15 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    app.use(express.static('client/build'));
+    app.get('*', (req, res, next) => {
+      res.sendFile(
+        path.resolve(
+          __dirname,
+
+          'client/build, index.html'
+        )
+      );
     });
   }
   app.use('/api/auth', auth);
