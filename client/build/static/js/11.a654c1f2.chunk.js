69884d08e8936f3b69aefef53a1dcb29d4eb3369
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-10-01 18:14:03 -0400

    build not uploading due to gitignore

--- client/build/static/js/11.a654c1f2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbudgeting-app"]=this["webpackJsonpbudgeting-app"]||[]).push([[11],{155:function(n,t,e){"use strict";e.r(t);var a=e(0),p=e.n(a);t.default=function(){return p.a.createElement(p.a.Fragment,null)}}}]);
-//# sourceMappingURL=11.a654c1f2.chunk.js.map
--- client/build/static/js/8.62a1d2ef.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbudgeting-app"]=this["webpackJsonpbudgeting-app"]||[]).push([[8],{156:function(n,t,e){"use strict";e.r(t);var a=e(0),p=e.n(a);t.default=function(n){return p.a.createElement(p.a.Fragment,null)}}}]);
-//# sourceMappingURL=8.62a1d2ef.chunk.js.map
