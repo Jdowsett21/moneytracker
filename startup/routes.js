40e916b093cc2aef989a8b506b85836f75c732cd
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-10-01 10:46:52 -0400

    fixing 500 error, no csrf token

--- startup/routes.js
+++ startup/routes.js
@@ -34,6 +34,12 @@ module.exports = function (app) {
   app.use(attachUser);
   app.use(verifyJwt);
   app.use(csrfProtection);
+  app.use('/api/users', users);
+  app.use('/api/accounts', accounts);
+  app.use('/api/settings', settings);
+  app.use('/api/transactions', transactions);
+  app.use('/api/budgets', budgets);
+  app.use('/api/budgetCategories', budgetCategories);
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('/*', function (req, res) {
@@ -47,10 +53,4 @@ module.exports = function (app) {
       );
     });
   }
-  app.use('/api/users', users);
-  app.use('/api/accounts', accounts);
-  app.use('/api/settings', settings);
-  app.use('/api/transactions', transactions);
-  app.use('/api/budgets', budgets);
-  app.use('/api/budgetCategories', budgetCategories);
 };
