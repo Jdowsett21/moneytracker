96a59397dcbdb2b9a81c52d45c49619a12f0ca5c
Jdowsett21 <james.dowsett.cooper@gmail.com>
2020-10-02 12:13:12 -0400

    fixing error

--- startup/routes.js
+++ startup/routes.js
@@ -42,15 +42,15 @@ module.exports = function (app) {
   app.use('/api/budgetCategories', budgetCategories);
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(
-        path.resolve(__dirname, '../client', 'build', 'index.html'),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-    });
+    // app.get('*', (req, res) => {
+    //   res.sendFile(
+    //     path.resolve(__dirname, '../client', 'build', 'index.html'),
+    //     function (err) {
+    //       if (err) {
+    //         res.status(500).send(err);
+    //       }
+    //     }
+    //   );
+    // });
   }
 };
